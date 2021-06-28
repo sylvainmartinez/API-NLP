5da4c95c0e9be07ce63ad4d2d87ed06b04a586b7
sylvainmartinez <martinez.sylv@gmail.com>
2021-06-28 10:16:31 +0200

    update model

---
+++ app/function.py
@@ -0,0 +1,63 @@
+import nltk
+from nltk.stem.snowball import FrenchStemmer, EnglishStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+import numpy as np
+from pickle import Unpickler as Upck
+
+# Récupération du modèle
+with open("dataCLSlabel2", 'rb') as file:
+    Upickler = Upck(file)
+    cls = Upickler.load() 
+
+# Récupération des Tags
+with open("dataAPIlabel2", 'rb') as file:
+    Upickler = Upck(file)
+    Tag = Upickler.load()
+
+
+def tokensLem(text):
+    lemmatizer = WordNetLemmatizer()
+    stemmer = EnglishStemmer()
+    tokenizer = nltk.RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    for j, word in enumerate(tokens):
+        tokens[j] = lemmatizer.lemmatize(word)
+    tokens = " ".join(tokens)
+    return tokens
+
+    
+def textRg(text):
+    '''Réponse a la question
+    proposition de Tag(s)'''
+    # en minuscule
+    question = text.lower()  
+
+    # Application de regex
+    question = re.sub(r'[^a-z\+\#\.\-]', ' ', question)
+    question = re.sub(r'([^c]\+{2,})| \++|\w+\++\w+', ' ', question)
+    question = re.sub(r'[^a-z]\-{1,}|\-{1,}[^a-z]', ' ', question)
+    question = re.sub(r'[^a-z+]\.{1,}|\.{1,}[^a-z]', ' ', question)
+    question = re.sub(r'([^c]\#{1,})', ' ', question)
+    question = re.sub(r'\w{30,}', ' ', question)
+    
+    # Pretraitement
+    question = tokensLem(question)
+    print('n', question)
+    # Prediction on prends en compte les 3 classes
+    # ayant les plus fortes probabilité
+    pred = cls.predict([question])[0]
+    pred_p = cls.predict_proba([question])[0]
+    pred = pred[pred_p.argsort()][::-1]
+    tag = Tag[pred_p.argsort()][::-1]
+    
+    if cls.predict([question])[0].sum() == 0:
+        return tag[0]
+    else:
+        i=0
+        ans = []
+        while pred[i] == 1:
+            ans.append(tag[i])
+            i += 1 
+        tag = " ".join(ans)
+        return tag
--- app/main.py
+++ app/main.py
@@ -2,70 +2,13 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 import os
-
-import nltk
-from nltk.stem.snowball import FrenchStemmer, EnglishStemmer
-from nltk.stem import WordNetLemmatizer
-import numpy as np
-import re
-from pickle import Unpickler as Upck
-import nltk
-from nltk.stem.snowball import FrenchStemmer, EnglishStemmer
-from nltk.stem import WordNetLemmatizer
+from app.function import *
 
 SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-'''
-def tokensLem(text):
-    lemmatizer = WordNetLemmatizer()
-    stemmer = EnglishStemmer()
-    tokenizer = nltk.RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(text)
-    for j, word in enumerate(tokens):
-        tokens[j] = lemmatizer.lemmatize(word)
-    return tokens'''
-
-with open("dataCLSlabel2", 'rb') as file:
-    Upickler = Upck(file)
-    cls_u = Upickler.load()
-    cls_s = Upickler.load() 
-    
-with open("dataAPIlabel2", 'rb') as file:
-    Upickler = Upck(file)
-    Tag = Upickler.load()
-
-def textRg(text, Tag=Tag):
-    
-    # en minuscule
-    question = text.lower()  
-
-    # Supprime les mots de 1 ou 2 lettre ou > 30 lettres
-    question = re.sub(r'[^a-z|c++|c#|node\.js|\.net|asp\.net|ruby\-on\-rails|sql\-server|objective\-c|android\-studio|visual\-studio]', ' ', question)
-    #question = re.sub(r'\.', ' ', question)
-    question = re.sub(r'(\-{2,})|( \-+ )|(\-+ )|( \-+)', ' ', question)
-    question = re.sub(r'( \++$)|( \++)|( \++ )|(^\++)', ' ', question)
-    question = re.sub(r'(^\.+)|( \.{2,})|( \.+ )|(\.+$)|(\.{2,})', ' ', question)
-    question = re.sub(r'\w{30,}', ' ', question)
-    
-    # Prediction on prends en compte les 3 classes
-    # ayant les plus fortes probabilité
-    pred = cls_s.predict([question])[0]
-    pred_p = cls_s.predict_proba([question])[0]
-    pred = pred[pred_p.argsort()][::-1]
-    Tag = Tag[pred_p.argsort()][::-1]
-    i=0
-    ans = []
-    while pred[i] == 1:
-        ans.append(Tag[i])
-        i += 1 
-    tag = " ".join(ans)
-    return tag
-
-############################################
-# User interface
 class GreetUserForm(FlaskForm):
     username = StringField(label=('Enter une question:'))
     submit = SubmitField(label=('Submit'))
