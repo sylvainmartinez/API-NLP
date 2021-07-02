ef447459dadf23037d125256772d88a2b8dfa457
sylvainmartinez <martinez.sylv@gmail.com>
2021-07-02 10:22:01 +0200

    Update template

--- app/main.py
+++ app/main.py
@@ -10,18 +10,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 class GreetUserForm(FlaskForm):
-    username1 = StringField(label=('Entrer une question:'))
+    question = StringField(label=('Entrer une question:'))
     submit = SubmitField(label=('Submit'))
-    username2 = StringField(label=('Tags suggéré(s)'))
+    reponse = StringField(label=('Tags suggéré(s)'))
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
    
     form = GreetUserForm()
     if form.validate_on_submit():
-        form.username2.data = textRg(form.username1.data)
-        #return f'''<h1> suggested Tag(s) : {form.username.data} </h1>'''
+        form.reponse.data = textRg(form.question.data)
     return render_template('index.html', form=form)
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
